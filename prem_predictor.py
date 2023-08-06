82342ff9f3178e81112987b124d18f5dbce06161
Brian Alonso <alonsobrian2@gmail.com>
2023-08-05 21:16:57 -0400

    Adding a Fixture class

--- prem_predictor.py
+++ prem_predictor.py
@@ -1,6 +1,7 @@
 import json
 import random
 from datetime import datetime
+from typing import Union
 
 CURRENT_YEAR = datetime.now().year
 
@@ -14,7 +15,7 @@ class Team:
     name = ""
 
     # optional display name to be consistent with PL website
-    display_name = ""
+    _display_name = ""
 
     # the probability of a win at home and away from home
     home_prob = 0.0
@@ -28,6 +29,7 @@ class Team:
     draws: int = 0
     losses: int = 0
     matches_played: int = 0
+
     # end of season stats
     finish: int = 0
     total_points: int = 0
@@ -36,41 +38,47 @@ class Team:
     total_losses: int = 0
 
     # the team can be constructed using these three parameters
-    def __init__(self, name_in, display_name_in, home_prob_in, away_prob_in):
-        self.name = name_in
-        self.display_name = display_name_in
-        self.home_prob = home_prob_in
-        self.away_prob = away_prob_in
-        self._starting_home_prob = home_prob_in
-        self._starting_away_prob = away_prob_in
-
-    # returns the home win probability
-    @property
-    def home(self):
-        return float(self.home_prob)
-
-    # returns the away win probability
-    @property
-    def away(self):
-        return float(self.away_prob)
+    def __init__(
+        self,
+        name: str,
+        display_name: Union[str, None],
+        home_prob: float,
+        away_prob: float,
+    ) -> None:
+        self.name = name
+        self._display_name = display_name
+        self.home_prob = home_prob
+        self.away_prob = away_prob
+        self._starting_home_prob = home_prob
+        self._starting_away_prob = away_prob
 
     # returns the display name or name if no display name
     @property
     def display_name(self):
-        return self.display_name if self.display_name else self.name
+        return self._display_name if self._display_name else self.name
 
-    # function to handle wins
+    def match_result(self, func: callable) -> None:
+        """Function to handle the result of a match
+
+        Args:
+            func (callable): The function to call to handle the result
+        """
+        func()
+        self.matches_played += 1
+
+    @match_result
     def win(self):
+        """Function to handle wins"""
         self.points += 3
         self.wins += 1
-        self.matches_played += 1
         if self.home_prob < 1:
             self.home_prob += FORM
         if self.away_prob < 1:
             self.away_prob += FORM
 
-    # function to handle losses
+    @match_result
     def lose(self):
+        """Function to handle losses"""
         self.losses += 1
         self.matches_played += 1
         if self.home_prob > 5 * FORM:
@@ -78,16 +86,12 @@ class Team:
         if self.away_prob > 5 * FORM:
             self.away_prob -= FORM
 
-    # function to handle draws
+    @match_result
     def draw(self):
+        """Function to handle draws"""
         self.points += 1
         self.draws += 1
         self.matches_played += 1
-
-    # reset a teams home and away probability to start a new season
-    def reset(self, home_prob, away_prob):
-        self.home_prob = home_prob
-        self.away_prob = away_prob
 
     # variable to keep track of table position
     def result(self, finish: int) -> None:
@@ -115,79 +119,114 @@ class Team:
         self.away_prob = self._starting_away_prob
 
 
-# function to parse my input file. Parameter is the line of the file,
-# returns a team
-def parse_team(to_parse: dict):
-    name = to_parse.get("name")
-    display_name = to_parse.get("display name")
-    home = float(to_parse.get("home"))
-    away = float(to_parse.get("away"))
-    return Team(name, display_name, home, away)
+# this class encapsulates a premier league fixture
+class Fixture:
+    # the home and away teams
+    home: Team
+    away: Team
+
+    # the match can be constructed using these three parameters
+    def __init__(self, home: Team, away: Team) -> None:
+        self.home = home
+        self.away = away
+
+    def play_match(self) -> None:
+        """Function to play the match"""
+        # Get the home and away probabilities
+        home = self.home.home_prob
+        away = self.away.away_prob
+
+        # computing the draw constant
+        # it should be less likely to draw if one team is really good and
+        # one team is really bad
+        # draw_const = 0.27 + home - away #/ (1 + home - away)
+        total = home + away
+        draw_const = 0.27 * abs(1 - home - away) / total
+
+        draw = 100 * (draw_const + home + away)
+        winner = random.randint(0, int(draw)) / 100.0
+
+        # logic to assign points
+        if winner < draw_const:
+            self.home.draw()
+            self.away.draw()
+        elif winner > draw_const and winner < draw_const + home:
+            self.home.win()
+            self.away.lose()
+        else:
+            self.home.lose()
+            self.away.win()
 
 
-def play_match(home_side: Team, away_side: Team) -> None:
-    # Get the home and away probabilities
-    home = home_side.home
-    away = away_side.away
+def build_teams() -> dict[str, Team]:
+    """Builds the teams for the season from the teams.json file
 
-    # computing the draw constant
-    # it should be less likely to draw if one team is really good and
-    # one team is really bad
-    # draw_const = 0.27 + home - away #/ (1 + home - away)
-    total = home + away
-    draw_const = 0.27 * abs(1 - home - away) / total
-
-    draw = 100 * (draw_const + home + away)
-    winner = random.randint(0, int(draw)) / 100.0
-
-    # logic to assign points
-    if winner < draw_const:
-        home_side.draw()
-        away_side.draw()
-    elif winner > draw_const and winner < draw_const + home:
-        home_side.win()
-        away_side.lose()
-    else:
-        home_side.lose()
-        away_side.win()
-
-
-def main():
-    # open fixtures file
-    with open(f"./{CURRENT_YEAR}-{CURRENT_YEAR + 1}/fixtures.json", "r") as f:
-        fixtures = json.load(f)
+    Returns:
+        dict[str, Team]: A dictionary of teams for the season
+    """
 
     # open the input file
     with open(f"./{CURRENT_YEAR}-{CURRENT_YEAR + 1}/teams.json", "r") as f:
         teams_raw: dict = json.load(f)
 
     # fill out the array of teams
-    teams: dict[str, Team] = {}
-    for i_team in teams_raw.values():
-        new_team = parse_team(i_team)
-        teams[new_team.name] = new_team
+    teams: dict[str, Team] = {
+        team["name"]: parse_team(team) for team in teams_raw.values()
+    }
 
-    # save season starting probabilities
-    start_prob = {}
-    for team_name, team in teams.items():
-        start_prob[team_name] = {"home": team.home, "away": team.away}
+    return teams
+
+
+def build_fixtures(teams: dict[str, Team]) -> list[Fixture]:
+    """Builds the fixtures for the season
+
+    Args:
+        teams (dict[str, Team]): Teams available for the season
+
+    Returns:
+        list[Fixture]: A list of fixtures for the season
+    """
+    # open fixtures file
+    with open(f"./{CURRENT_YEAR}-{CURRENT_YEAR + 1}/fixtures.json", "r") as f:
+        fixtures_raw = json.load(f)
+
+    # fill out the array of fixtures
+    fixtures: list[Fixture] = [
+        Fixture(teams.get(fixture["HomeTeam"]), teams.get(fixture["AwayTeam"]))
+        for fixture in fixtures_raw
+    ]
+
+    return fixtures
+
+
+# function to parse my input file. Parameter is the line of the file,
+# returns a team
+def parse_team(to_parse: dict):
+    name = to_parse["name"]
+    display_name = to_parse["display name"]
+    home = float(to_parse["home"])
+    away = float(to_parse["away"])
+    return Team(name, display_name, home, away)
+
+
+def main():
+    # build the teams and fixtures
+    teams = build_teams()
+    fixtures = build_fixtures(teams)
 
     # variable to set how many seasons to sim
     sims: int = 20
+
+    # simulating the seasons
     for _ in range(sims):
         for fixture in fixtures:
-            home_side: Team = teams.get(fixture["HomeTeam"])
-            away_side: Team = teams.get(fixture["AwayTeam"])
-
             # using a random number generator to determine the winner
-            play_match(home_side, away_side)
-
-        for team_name, probs in start_prob.items():
-            teams.get(team_name).reset(probs["home"], probs["away"])
+            fixture.play_match()
 
         # reset the teams for the next 'season' or simulation
         final_order = sorted(teams.values(), reverse=True, key=lambda team: team.points)
-        for team_name, team in teams.items():
+
+        for team in teams.values():
             # Set the team's final position
             team.result(final_order.index(team) + 1)
 
