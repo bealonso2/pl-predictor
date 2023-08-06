095f4d9839a2740afaac7b67c6bfb20c42badf68
Brian Alonso <alonsobrian2@gmail.com>
2023-08-05 20:48:42 -0400

    More reformatting, next up is teams

--- prem_predictor.py
+++ prem_predictor.py
@@ -1,5 +1,8 @@
 import json
 import random
+from datetime import datetime
+
+CURRENT_YEAR = datetime.now().year
 
 # adding functionality for 'FORM' by using fixtures. Right now fixtures won't change
 FORM = 0.005
@@ -16,6 +19,8 @@ class Team:
     # the probability of a win at home and away from home
     home_prob = 0.0
     away_prob = 0.0
+    _starting_home_prob = 0.0
+    _starting_away_prob = 0.0
 
     # the teams stats
     points: int = 0
@@ -36,37 +41,22 @@ class Team:
         self.display_name = display_name_in
         self.home_prob = home_prob_in
         self.away_prob = away_prob_in
+        self._starting_home_prob = home_prob_in
+        self._starting_away_prob = away_prob_in
 
     # returns the home win probability
-    def getHome(self):
+    @property
+    def home(self):
         return float(self.home_prob)
 
     # returns the away win probability
-    def getAway(self):
+    @property
+    def away(self):
         return float(self.away_prob)
 
-    # returns the number of points
-    def getPoints(self):
-        return self.points
-
-    # returns the number of wins
-    def getWs(self):
-        return self.wins
-
-    # returns the number of draws
-    def getDraws(self):
-        return self.draws
-
-    # returns the number of losses
-    def getLs(self):
-        return self.losses
-
-    # returns the number of matches played
-    def getMP(self):
-        return self.matches_played
-
     # returns the display name or name if no display name
-    def getDisplayName(self):
+    @property
+    def display_name(self):
         return self.display_name if self.display_name else self.name
 
     # function to handle wins
@@ -100,19 +90,32 @@ class Team:
         self.away_prob = away_prob
 
     # variable to keep track of table position
-    def result(self, finish):
+    def result(self, finish: int) -> None:
+        """Function to reset the teams stats and add previous season stats to
+        cumulative stats
+
+        Args:
+            finish (int): The position the team finished in the table
+        """
+        # Set the fields that are cumulative
         self.finish += finish
         self.total_points += self.points
         self.total_wins += self.wins
         self.total_draws += self.draws
         self.total_losses += self.losses
+
+        # Reset the team stats
         self.points = 0
         self.wins = 0
         self.draws = 0
         self.losses = 0
 
+        # Reset the team with the starting probabilities
+        self.home_prob = self._starting_home_prob
+        self.away_prob = self._starting_away_prob
 
-# function to parse my input file. Paraneter is the line of the file,
+
+# function to parse my input file. Parameter is the line of the file,
 # returns a team
 def parse_team(to_parse: dict):
     name = to_parse.get("name")
@@ -122,79 +125,89 @@ def parse_team(to_parse: dict):
     return Team(name, display_name, home, away)
 
 
-# open fixtures file
-with open("./2022-2023/2022-2023_fixtures.json", "r") as f:
-    fixtures = json.load(f)
+def play_match(home_side: Team, away_side: Team) -> None:
+    # Get the home and away probabilities
+    home = home_side.home
+    away = away_side.away
 
-# open the input file
-with open("./2022-2023/2022-2023_teams.json", "r") as f:
-    teams_raw: dict = json.load(f)
+    # computing the draw constant
+    # it should be less likely to draw if one team is really good and
+    # one team is really bad
+    # draw_const = 0.27 + home - away #/ (1 + home - away)
+    total = home + away
+    draw_const = 0.27 * abs(1 - home - away) / total
 
-# fill out the array of teams
-teams = {}
-for i_team in teams_raw.values():
-    new_team = parse_team(i_team)
-    teams[new_team.name] = new_team
+    draw = 100 * (draw_const + home + away)
+    winner = random.randint(0, int(draw)) / 100.0
 
-# close the file to prevent data leaks
-f.close()
+    # logic to assign points
+    if winner < draw_const:
+        home_side.draw()
+        away_side.draw()
+    elif winner > draw_const and winner < draw_const + home:
+        home_side.win()
+        away_side.lose()
+    else:
+        home_side.lose()
+        away_side.win()
 
-# save season starting probablilties
-start_prob = {}
-for team_name, team in teams.items():
-    start_prob[team_name] = {"home": team.getHome(), "away": team.getAway()}
 
-# variable to set how many seasons to sim
-sims: int = 20
-for x in range(sims):
-    for fixture in fixtures:
-        home_side = teams.get(fixture.get("HomeTeam"))
-        away_side = teams.get(fixture.get("AwayTeam"))
+def main():
+    # open fixtures file
+    with open(f"./{CURRENT_YEAR}-{CURRENT_YEAR + 1}/fixtures.json", "r") as f:
+        fixtures = json.load(f)
 
-        home = home_side.getHome()
-        away = away_side.getAway()
-        # computing the draw constant
-        # it should be less likely to draw if one team is really good and
-        # one team is really bad
-        # draw_const = 0.27 + home - away #/ (1 + home - away)
-        total = home + away
-        draw_const = 0.27 * abs(1 - home - away) / total
-        draw = 100 * (draw_const + home + away)
+    # open the input file
+    with open(f"./{CURRENT_YEAR}-{CURRENT_YEAR + 1}/teams.json", "r") as f:
+        teams_raw: dict = json.load(f)
 
-        # using a random number generator to determine the winner
-        winner = random.randint(0, int(draw)) / 100.0
+    # fill out the array of teams
+    teams: dict[str, Team] = {}
+    for i_team in teams_raw.values():
+        new_team = parse_team(i_team)
+        teams[new_team.name] = new_team
 
-        # logic to assign points
-        if winner < draw_const:
-            home_side.draw()
-            away_side.draw()
-        elif winner > draw_const and winner < draw_const + home:
-            home_side.win()
-            away_side.lose()
-        else:
-            home_side.lose()
-            away_side.win()
-
-    for team_name, probs in start_prob.items():
-        teams.get(team_name).reset(probs.get("home"), probs.get("away"))
-
-    final_order = sorted(teams.values(), reverse=True, key=lambda team: team.points)
+    # save season starting probabilities
+    start_prob = {}
     for team_name, team in teams.items():
-        team.result(final_order.index(team) + 1)
+        start_prob[team_name] = {"home": team.home, "away": team.away}
+
+    # variable to set how many seasons to sim
+    sims: int = 20
+    for _ in range(sims):
+        for fixture in fixtures:
+            home_side: Team = teams.get(fixture["HomeTeam"])
+            away_side: Team = teams.get(fixture["AwayTeam"])
+
+            # using a random number generator to determine the winner
+            play_match(home_side, away_side)
+
+        for team_name, probs in start_prob.items():
+            teams.get(team_name).reset(probs["home"], probs["away"])
+
+        # reset the teams for the next 'season' or simulation
+        final_order = sorted(teams.values(), reverse=True, key=lambda team: team.points)
+        for team_name, team in teams.items():
+            # Set the team's final position
+            team.result(final_order.index(team) + 1)
+
+    # output to a table
+    # header
+    print(f"\n{'': <3} {'Team':<20s} Finish  Points  {'W':>4}  {'D':>4}  {'L':>4}")
+    print(f"{'':-<57}")
+    # sort the teams by points
+    final_order = sorted(teams.values(), reverse=False, key=lambda team: team.finish)
+    # output the teams
+    [
+        print(
+            f"{str(final_order.index(team) + 1) + ')':<3} {team.display_name:<20.20s} {team.finish/sims:>6.1f}  {team.total_points/sims:>6.1f}  {team.total_wins/sims:4.1f}  {team.total_draws/sims:4.1f}  {team.total_losses/sims:4.1f}"
+        )
+        for team in final_order
+    ]
+    print()
+
+    # TODO json output as well, maybe csv too?
 
 
-# output into a table
-print()
-# header
-print(f"{'': <3} {'Team':<20s} Finish  Points  {'W':>4}  {'D':>4}  {'L':>4}")
-print(f"{'':-<57}")
-# sort the teams by points
-final_order = sorted(teams.values(), reverse=False, key=lambda team: team.finish)
-# output the teams
-[
-    print(
-        f"{str(final_order.index(team) + 1) + ')':<3} {team.getDisplayName():<20.20s} {team.finish/sims:>6.1f}  {team.total_points/sims:>6.1f}  {team.total_wins/sims:4.1f}  {team.total_draws/sims:4.1f}  {team.total_losses/sims:4.1f}"
-    )
-    for team in final_order
-]
-print()
+if __name__ == "__main__":
+    main()
